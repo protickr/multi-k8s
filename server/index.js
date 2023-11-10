95298675fa010b446e8ca45c6d3b209d017f344e
Protick Roy <protickrr@gmail.com>
2023-11-10 22:08:45 +0600

    server fix test #1

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const pgClient = new Pool({
   database: keys.pgDatabase,
   password: keys.pgPassword,
   port: keys.pgPort,
-  ssl: {rejectUnauthorized: false }
+  ssl: process.env.NODE_ENV !== 'production' ? false : { rejectUnauthorized: false },
 });
 
 pgClient.on("connect", (client) => {
